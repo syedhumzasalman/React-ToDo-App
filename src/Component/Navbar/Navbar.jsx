d04fa5867da87046799d9e199552abf4239fe23e
Syed Humza <105381562+syedhumzasalman@users.noreply.github.com>
2025-08-30 03:49:12 +0500

    Update Navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -10,11 +10,6 @@ export const Navbar = () => {
 
                 {/* Logo */}
                 <a  className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img
-                        src="/public/todofavicon.png"
-                        className="h-8"
-                        alt="Flowbite Logo"
-                    />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                         ToDo List
                     </span>
